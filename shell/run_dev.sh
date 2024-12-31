91bdfbe4c9e76aea161b55ebbf156feae8c418bb
Rob Wood <metathinker1@gmail.com>
2024-12-31 14:31:22 -0700

    Add ANCESTRY_DOMAIN_ENTITIES_FILE_NAME to run scripts

--- shell/run_dev.sh
+++ shell/run_dev.sh
@@ -3,6 +3,7 @@ source ../venv/bin/activate
 # export PYTHONPATH="/Users/robertwood/Project.ThoughtPal/notedoc-service/venv/bin"
 export PYTHONPATH="/Users/robertwood/Project.ThoughtPal/notedoc-service"
 export NOTEDOC_ACTIVE_ENTITIES_FILE="/Users/robertwood/Project.ThoughtPal/NoteDocExtracts/ActiveEntities.json"
+export ANCESTRY_DOMAIN_ENTITIES_FILE_NAME="/Users/robertwood/Project.ThoughtPal/NoteDocExtracts/Domain.Ancestry.json"
 export NOTEDOC_FILE_REPO_PATH="/Users/robertwood/Project.ThoughtPal/AncNoteDocRepo"
 export PERSON_REPO_FILE="/Users/robertwood/Project.ThoughtPal/NoteDocExtracts/PersonRepo.json"
 python3 ../notesrvc/app.py
--- shell/run_prod.sh
+++ shell/run_prod.sh
@@ -2,6 +2,7 @@
 source ../venv/bin/activate
 export PYTHONPATH="/Users/rwood/Tools/notedoc-service"
 export NOTEDOC_ACTIVE_ENTITIES_FILE="/Users/rwood/Tools/NoteDocOrganizer/ActiveEntities.json"
+export ANCESTRY_DOMAIN_ENTITIES_FILE_NAME="/Users/rwood/Tools/NoteDocOrganizer//Domain.Ancestry.json"
 export NOTEDOC_FILE_REPO_PATH="/Users/rwood/NoteDocRepo"
 export PERSON_REPO_FILE="/Users/rwood/Tools/NoteDocOrganizer/PersonRepo.json"
 python3 ../notesrvc/app.py
